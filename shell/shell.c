efc97e245d09b3394a40ea3cc040a9cbf389e28f
jackeyhua <dyhzd2013@163.com>
2024-03-12 19:18:30 +0800

    feat: support `pipe` in shell

--- shell/shell.c
+++ shell/shell.c
@@ -10,7 +10,8 @@
 #define MAX_ARGS 1024
 
 void init_name_and_path(char[], char[]);
-void parse_input(char[], char**);
+int parse_input(char[], char**);
+void exec(char**, int, int);
 
 // TODO: support pipe
 // Feat: 1. run single shell command
@@ -21,30 +22,77 @@ int main() {
     char path[MAX_PATH];
     char input[MAX_INPUT];
     init_name_and_path(name, path);
-
+    
+    // TODO: fix bug in while(1)
     while (1) {
         /* change the color of info into what real bash is like */
         printf("\033[1;32m%s\033[0m:\033[1;34m%s\033[0m$ ", name, path);
         fgets(input, MAX_INPUT, stdin);
         char** argv = malloc(MAX_ARGS * sizeof(char*));
-        parse_input(input, argv);
-
-        if (fork() == 0) {
-            /* absolute path for shell command */
-            char* bin = malloc(MAX_NAME * sizeof(char));
-            sprintf(bin, "/bin/%s", argv[0]);
-            execv(bin, argv);
-            free(bin);
-        } else {
-            wait(0);
-        }
+        
+        int argc = parse_input(input, argv);
+        
+        // if (fork() == 0) {
+        //     /* absolute path for shell command */
+        //     char* bin = malloc(MAX_NAME * sizeof(char));
+        //     sprintf(bin, "/bin/%s", argv[0]);
+        //     execv(bin, argv);
+        //     free(bin);
+        // } else {
+        //     wait(0);
+        // }
+        exec(argv, 0, argc - 1);
         free(argv);   
     }
 
     return 0;
 }
 
-void parse_input(char input[], char** argv) {
+void exec(char** argv, int lb, int rb) {
+    // valid range: [lb, rb]
+    /* check whether there is a pipe */
+    int possible_right = -1;
+    for (int i = lb; i <= rb; i++) {
+        if (strcmp(argv[i], "|") == 0) {
+            possible_right = i;
+            break;
+        }
+    }
+    printf("possible_right: %d\n", possible_right);
+    if (possible_right == -1) {
+        if (fork() == 0) {
+            execvp(argv[lb], argv + lb);
+        } else {
+            wait(0);
+        }
+    } else { 
+        int p[2];
+        pipe(p);
+        if (fork() == 0) {
+            close(p[0]);
+            close(1);
+            dup(p[1]);
+            close(p[1]);
+            // char** new_argv[possible_right - lb + 1];
+            // for (int i = lb; i < possible_right; i++) {
+            //     *new_argv[i - lb] = argv[i];
+            // }
+            // new_argv[possible_right - lb] = NULL;
+            argv[possible_right] = NULL;
+            execvp(argv[lb], argv + lb);
+        } else {
+            wait(0);
+            close(p[1]);
+            close(0);
+            dup(p[0]);
+            close(p[0]);
+            exec(argv, possible_right + 1, rb);
+        }
+    } 
+}
+
+
+int parse_input(char input[], char** argv) {
     char* p = strchr(input, '\n');
     if (p != NULL) {
         *p = '\0';
@@ -55,6 +103,8 @@ void parse_input(char input[], char** argv) {
         argv[i++] = token;
         token = strtok(NULL, " ");
     }
+    argv[i] = NULL;
+    return i;
 }
 
 void init_name_and_path(char name[], char path[]) {
