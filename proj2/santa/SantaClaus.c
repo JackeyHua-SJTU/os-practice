76ae5a78766e73a4321593d0d23d85bf8142cd34
jackeyhua <dyhzd2013@163.com>
2024-04-12 16:31:13 +0800

    feat: add redundacy check and sync mutex

--- proj2/santa/SantaClaus.c
+++ proj2/santa/SantaClaus.c
@@ -9,26 +9,35 @@
 #define NUM_ELVES_FOR_SANTA 3
 #define ROUNDS 3
 #define SLEEP 5
+#define RED "\x1B[31m"
+#define RESET "\x1B[0m"
+#define GREEN "\x1B[32m"
+#define YELLOW "\x1B[33m"
+#define MAGENTA "\x1B[35m"
+
 
 sem_t santaSem, reindeerSem, elfSem;                // 唤醒圣诞老人的信号量、让驯鹿系上绳子的信号量和让精灵接受帮助的信号量
 sem_t elfallow;                                     // 当有三只精灵在等待后，不再允许精灵进入等待队列
+sem_t reindeerEnd;                                  // 巡回是否结束的信号量
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  // 用于限制访问资源的互斥锁
 int reindeerCount = 0, elfCount = 0;                // 存储精灵和驯鹿的个数
 int sleighCount = 0, helpCount = 0;                 // 存储圣诞老人出去送礼物/帮助精灵的次数
+int hitchedCount = 0;                               // 存储已经系上绳索的驯鹿的个数
 int elvesBuf[NUM_ELVES_FOR_SANTA];                  // 存储需要帮助的三只精灵的id
 int flag_r = 1;                                     // 在圣诞老人巡回结束后，必须9只驯鹿全部释放资源后，才能有新的驯鹿进来等待
 int flag_e = 1;                                     // 在圣诞老人帮完三个精灵后，必须三个精灵全部释放资源后，才能有新的精灵进来等待
 int flag_exit = 0;                                  // 当循环条件满足的时候，存在部分精灵仍在等待，需要手动的去释放
-
-// TODO: add random sleep
+int santa_end = 0;                                  // 当前正在接受圣诞老人服务的精灵/驯鹿必须等待圣诞老人结束后才可释放
 
 void* SantaClaus(void* arg) {
     while (1) {
         int f = 0;
         pthread_mutex_lock(&mutex);
         if (sleighCount >= ROUNDS && helpCount >= ROUNDS) {
+            printf(YELLOW "Santa Claus DONE.\n" RESET);
             f = 1;
             flag_exit = 1;
+            /* 释放信号量资源，防止精灵忙等 */
             for (int i = 0; i < NUM_ELVES_FOR_SANTA; ++i) sem_post(&elfSem);
             for (int i = 0; i < NUM_ELVES; ++i) sem_post(&elfallow);
         }
@@ -36,20 +45,19 @@ void* SantaClaus(void* arg) {
         if (f) break;
 
         sem_wait(&santaSem);
-        int rc, ec;
+        printf(MAGENTA "Santa Claus is waken up.\n" RESET);
+        int rc, ec, sc, hc;
 
         pthread_mutex_lock(&mutex);
         rc = reindeerCount;
         ec = elfCount;
-        pthread_mutex_unlock(&mutex);
-        int sc, hc;
-        pthread_mutex_lock(&mutex);
         sc = sleighCount;
         hc = helpCount;
         pthread_mutex_unlock(&mutex);
-        // Santa gives priority to the reindeer.
+
+        /* 优先处理驯鹿 */
         if (rc == NUM_REINDEER) {
-            printf("Santa Claus: preparing sleigh.\nSleigh count : %d, Help elves count : %d.\n", sc + 1, hc);
+            printf(RED "Santa Claus: preparing sleigh.\nSleigh count : %d, Help elves count : %d.\n" RESET, sc + 1, hc);
             pthread_mutex_lock(&mutex);
             flag_r = 0;
             sleighCount++;
@@ -57,8 +65,20 @@ void* SantaClaus(void* arg) {
             for (int i = 0; i < NUM_REINDEER; i++) {
                 sem_post(&reindeerSem);
             }
-            // TODO: add sync here
-            printf("Santa Claus: make all kids in the world happy\n");
+
+            while (1) {
+                int cnt;
+                pthread_mutex_lock(&mutex);
+                cnt = hitchedCount;
+                pthread_mutex_unlock(&mutex);
+                if (cnt == NUM_REINDEER) break;
+            }
+
+            printf(GREEN "Santa Claus together with %d reindeers are going to send gifts to children around the world.\n" RESET, NUM_REINDEER);
+            sleep(rand() % SLEEP);              // 模拟送礼物
+            printf(MAGENTA "Santa Claus finishes sending gift to children.\n" RESET);
+            for (int i = 0; i < NUM_REINDEER; ++i) sem_post(&reindeerEnd);
+            /* 模拟结束，释放驯鹿资源 */
             while (1) {
                 int cnt;
                 pthread_mutex_lock(&mutex);
@@ -68,17 +88,24 @@ void* SantaClaus(void* arg) {
             }
             pthread_mutex_lock(&mutex);
             flag_r = 1;
+            hitchedCount = 0;
             pthread_mutex_unlock(&mutex);
         } else if (ec == NUM_ELVES_FOR_SANTA) {
-            printf("Santa Claus: helping elves.\nSleigh count : %d, Help elves count : %d.\n", sc, hc + 1);
+            printf(RED "Santa Claus: helping elves.\nSleigh count : %d, Help elves count : %d.\n" RESET, sc, hc + 1);
             pthread_mutex_lock(&mutex);
             flag_e = 0;
             helpCount++;
             pthread_mutex_unlock(&mutex);
             for (int i = 0; i < NUM_ELVES_FOR_SANTA; i++) {
-                printf("Elf %d is being helped.\n", elvesBuf[i]);
+                printf(GREEN "Elf %d is being helped.\n" RESET, elvesBuf[i]);
+                // sem_post(&elfSem);  // 因为 flag_e 的存在，即使被帮助的精灵已经进入下一个循环了，也会在原地死循环
+            }
+            sleep(rand() % SLEEP);
+            printf(MAGENTA "Santa Claus finishes helping the elves.\n" RESET);
+            for (int i = 0; i < NUM_ELVES_FOR_SANTA; ++i) {
                 sem_post(&elfSem);
             }
+
             while (1) {
                 int cnt;
                 pthread_mutex_lock(&mutex);
@@ -92,7 +119,6 @@ void* SantaClaus(void* arg) {
             for (int i = 0; i < NUM_ELVES_FOR_SANTA; ++i) sem_post(&elfallow);
         }
     }
-    printf("Santa Claus DONE.\n");
     return NULL;
 }
 
@@ -116,12 +142,17 @@ void* Reindeer(void* arg) {
         }
         pthread_mutex_unlock(&mutex);
         sem_wait(&reindeerSem);
-        printf("Reindeer %d getting hitched.\n", id);
+        printf(GREEN "Reindeer %d getting hitched.\n" RESET, id);
+        pthread_mutex_lock(&mutex);
+        ++hitchedCount;
+        pthread_mutex_unlock(&mutex);
+        sem_wait(&reindeerEnd);
         pthread_mutex_lock(&mutex);
         --reindeerCount;
+        // printf("Reindeer %d leaves, current count : %d.\n", id, reindeerCount);
         pthread_mutex_unlock(&mutex);
     }
-    printf("Reindeer %d DONE.\n", *(int*)arg);
+    printf(YELLOW "Reindeer %d DONE.\n" RESET, *(int*)arg);
     return NULL;
 }
 
@@ -134,8 +165,9 @@ void* Elf(void* arg) {
         pthread_mutex_unlock(&mutex);
         if (f) break;
         if (!f1) continue;
-
+        
         sleep(rand() % SLEEP);
+        // printf("Elf %d is waiting for the santa claus.\n", *(int*)arg);
         sem_wait(&elfallow);
         int nf;
         pthread_mutex_lock(&mutex);
@@ -143,7 +175,7 @@ void* Elf(void* arg) {
         pthread_mutex_unlock(&mutex);
 
         if (nf) break;
-
+        
         int id = *(int*)arg;
         // printf("This is elf %d\n", id);
         pthread_mutex_lock(&mutex);
@@ -153,6 +185,7 @@ void* Elf(void* arg) {
             ++elfCount;
             if (elfCount == NUM_ELVES_FOR_SANTA) sem_post(&santaSem);
         } else {
+            // 因为 elfallow 资源的限制，永远不会执行到
             printf("Elf %d goes back.\n", id);
             continue;
         }
@@ -162,7 +195,7 @@ void* Elf(void* arg) {
         --elfCount;
         pthread_mutex_unlock(&mutex);       
     }
-    printf("Elf %d DONE.\n", *(int*)arg);
+    printf(YELLOW "Elf %d DONE.\n" RESET, *(int*)arg);
     return NULL;
 }
 
@@ -170,30 +203,87 @@ int main() {
     pthread_t santa, reindeers[NUM_REINDEER], elves[NUM_ELVES];
     int ids[NUM_REINDEER > NUM_ELVES ? NUM_REINDEER : NUM_ELVES];
 
-    sem_init(&santaSem, 0, 0);
-    sem_init(&reindeerSem, 0, 0);
-    sem_init(&elfSem, 0, 0);
-    sem_init(&elfallow, 0, NUM_ELVES_FOR_SANTA);
+    if (sem_init(&santaSem, 0, 0) == -1) {
+        printf("Failed to initialize santaSem.\n");
+        exit(1);
+    }
+    if (sem_init(&reindeerSem, 0, 0) == -1) {
+        printf("Failed to initialize reindeerSem.\n");
+        exit(1);
+    }
+    if (sem_init(&elfSem, 0, 0) == -1) {
+        printf("Failed to initialize elfSem.\n");
+        exit(1);
+    }
+    if (sem_init(&elfallow, 0, NUM_ELVES_FOR_SANTA) == -1) {
+        printf("Failed to initialize elfallow.\n");
+        exit(1);
+    }
+    if (sem_init(&reindeerEnd, 0, 0) == -1) {
+        printf("Failed to initialize reindeerEnd.\n");
+        exit(1);
+    }
 
-    pthread_create(&santa, NULL, SantaClaus, NULL);
+    if (pthread_create(&santa, NULL, SantaClaus, NULL) != 0) {
+        printf("Failed to create santa claus thread.\n");
+        exit(1);
+    }
+
     for (int i = 0; i < NUM_REINDEER; i++) {
         ids[i] = i;
-        pthread_create(&reindeers[i], NULL, Reindeer, &ids[i]);
+        if (pthread_create(&reindeers[i], NULL, Reindeer, &ids[i]) != 0) {
+            printf("Failed to create reindeer thread #%d\n", i);
+            exit(1);
+        }
     }
     for (int i = 0; i < NUM_ELVES; i++) {
         ids[i] = i;
-        pthread_create(&elves[i], NULL, Elf, &ids[i]);
+        if (pthread_create(&elves[i], NULL, Elf, &ids[i]) != 0) {
+            printf("Failed to create elf thread #%d\n", i);
+            exit(1);
+        }
     }
 
-    pthread_join(santa, NULL);
+    if (pthread_join(santa, NULL) != 0) {
+        printf("Failed to join santa thread\n");
+        exit(1);
+    }
     for (int i = 0; i < NUM_REINDEER; i++) {
-        pthread_join(reindeers[i], NULL);
+        if (pthread_join(reindeers[i], NULL) != 0) {
+            printf("Failed to join reindeer thread #%d\n", i);
+            exit(1);
+        }
     }
     for (int i = 0; i < NUM_ELVES; i++) {
-        pthread_join(elves[i], NULL);
+        if (pthread_join(elves[i], NULL) != 0) {
+            printf("Failed to join elf thread #%d\n", i);
+            exit(1);
+        }
     }
 
-    // TODO: destroy the semaphore
-
+    if (sem_destroy(&santaSem) == -1) {
+        printf("Failed to destroy santaSem.\n");
+        exit(1);
+    }
+    if (sem_destroy(&reindeerSem) == -1) {
+        printf("Failed to destroy reindeerSem.\n");
+        exit(1);
+    }
+    if (sem_destroy(&elfSem) == -1) {
+        printf("Failed to destroy elfSem.\n");
+        exit(1);
+    }
+    if (sem_destroy(&elfallow) == -1) {
+        printf("Failed to destroy elfallow.\n");
+        exit(1);
+    }
+    if (sem_destroy(&reindeerEnd) == -1) {
+        printf("Failed to destroy reindeerEnd.\n");
+        exit(1);
+    }
+    if (pthread_mutex_destroy(&mutex) != 0) {
+        printf("Failed to destroy mutex.\n");
+        exit(1);
+    }
     return 0;
 }
